f452c8ee14db22712dc38c2a36b9a1e91e32cbfb
ShilkinYuV <51208697+ShilkinYuV@users.noreply.github.com>
2022-01-19 15:49:42 +0300

    хя

--- main.py
+++ main.py
@@ -35,74 +35,77 @@ class OperoPDS(QtWidgets.QMainWindow):
         self.ui.ZVPSEND.clicked.connect(lambda: self.sending(self.ZVPSEND))
 
     def sending(self, type):
-        files = os.listdir(self.isASFK)
-
+        dirsASFKPUDS = [self.isASFK, self.isPUDS]
         isEmpty = True
+        for currentdirs in dirsASFKPUDS:
 
-        for file in files:
-            myFile = self.isASFK + '\\' + file
-            mydoc = minidom.parse(myFile)
-            items = mydoc.getElementsByTagName('sen:Object')
+            files = os.listdir(currentdirs)
 
-            for elem in items:
-                self.elementXML = str(elem.firstChild.data)
-                if self.elementXML.__contains__(type):
-                    self.ui.textEdit.append('Начало копирования ' + file + ' в ' + self.inBANK)
-                    isEmpty = False
-                    try:
-                        shutil.copy2(myFile, self.inBANK + '\\' + file)
-                        self.ui.textEdit.append('Копирование ' + file + ' в ' + self.inBANK + ' успешно завершено')
-                    except Exception:
-                        self.ui.textEdit.append('Копирование ' + file + ' в ' + self.inBANK + ' не удалось')
+            for file in files:
+                myFile = currentdirs + '\\' + file
+                mydoc = minidom.parse(myFile)
+                items = mydoc.getElementsByTagName('sen:Object')
 
-                    bankfiles = os.listdir(self.inBANK)
-                    for bankfile in bankfiles:
-                        if bankfile.__contains__(file):
-                            self.ui.textEdit.append('Файл ' + file + ' присутствует в ' + self.inBANK)
-                            try:
-                               os.remove(myFile)
-                               self.ui.textEdit.append('Файл ' + file + ' удален из ' + self.isASFK)
-                            except:
-                                self.ui.textEdit.append('Не удалось удалить ' + file + ' из ' + self.isASFK)
+                for elem in items:
+                    self.elementXML = str(elem.firstChild.data)
+                    if self.elementXML.__contains__(type):
+                        self.ui.textEdit.append('       Начало копирования ' + file + ' в ' + self.inBANK)
+                        isEmpty = False
+                        try:
+                            shutil.copy2(myFile, self.inBANK + '\\' + file)
+                            self.ui.textEdit.append('Копирование ' + file + ' из ' + currentdirs +' в ' + self.inBANK + ' успешно завершено')
+                        except Exception:
+                            self.ui.textEdit.append('Копирование ' + file + ' из ' + currentdirs + ' в ' + self.inBANK + ' не удалось')
+
+                        bankfiles = os.listdir(self.inBANK)
+                        for bankfile in bankfiles:
+                            if bankfile.__contains__(file):
+                                self.ui.textEdit.append('Файл ' + file + ' присутствует в ' + self.inBANK)
+                                try:
+                                    os.remove(myFile)
+                                    self.ui.textEdit.append('Файл ' + file + ' удален из ' + currentdirs)
+                                except:
+                                    self.ui.textEdit.append('Не удалось удалить ' + file + ' из ' + currentdirs)
 
         if isEmpty:
             sender = self.sender()
-            self.ui.textEdit.append('Корневой каталог не содержит файлов ' + sender.text())
-
-
+            self.ui.textEdit.append('Корневые каталоги не содержат файлов ' + sender.text())
 
     def checkfiles(self):
-        files = os.listdir(self.isASFK)
-        print(files)
+        dirsASFKPUDS = [self.isASFK, self.isPUDS]
 
-        for file in files:
-            myFile = self.isASFK + '\\' + file
-            mydoc = minidom.parse(myFile)
-            items = mydoc.getElementsByTagName('sen:Object')
+        for currentdirs in dirsASFKPUDS:
+            files = os.listdir(currentdirs)
+            print(files)
 
-            for elem in items:
-                self.elementXML = str(elem.firstChild.data)
-                if self.elementXML.__contains__(self.OTVSEND):
-                    print('OTVSEND ' + file)
-                    self.ui.textEdit.setPlainText('OTVSEND ' + file)
-                elif self.elementXML.__contains__(self.OTVSEND):
-                    print('OTZVSEND ' + file)
-                    self.ui.textEdit.append('OTZVSEND ' + file)
-                elif self.elementXML.__contains__(self.PESSEND):
-                    print('PESSEND ' + file)
-                    self.ui.textEdit.append('PESSEND ' + file)
-                elif self.elementXML.__contains__(self.RNPSEND):
-                    print('RNPSEND ' + file)
-                    self.ui.textEdit.append('RNPSEND ' + file)
-                elif self.elementXML.__contains__(self.ZINFSEND):
-                    print('ZINFSEND ' + file)
-                    self.ui.textEdit.append('ZINFSEND ' + file)
-                elif self.elementXML.__contains__(self.ZONDSEND):
-                    print('ZONDSEND ' + file)
-                    self.ui.textEdit.append('ZONDSEND ' + file)
-                elif self.elementXML.__contains__(self.ZVPSEND):
-                    print('ZVPSEND ' + file)
-                    self.ui.textEdit.append('ZVPSEND ' + file)
+            for file in files:
+                myFile = currentdirs + '\\' + file
+                mydoc = minidom.parse(myFile)
+                items = mydoc.getElementsByTagName('sen:Object')
+
+                for elem in items:
+                    self.elementXML = str(elem.firstChild.data)
+                    if self.elementXML.__contains__(self.OTVSEND):
+                        print('OTVSEND ' + file)
+                        self.ui.textEdit.append('OTVSEND ' + file)
+                    elif self.elementXML.__contains__(self.OTVSEND):
+                        print('OTZVSEND ' + file)
+                        self.ui.textEdit.append('OTZVSEND ' + file)
+                    elif self.elementXML.__contains__(self.PESSEND):
+                        print('PESSEND ' + file)
+                        self.ui.textEdit.append('PESSEND ' + file)
+                    elif self.elementXML.__contains__(self.RNPSEND):
+                        print('RNPSEND ' + file)
+                        self.ui.textEdit.append('RNPSEND ' + file)
+                    elif self.elementXML.__contains__(self.ZINFSEND):
+                        print('ZINFSEND ' + file)
+                        self.ui.textEdit.append('ZINFSEND ' + file)
+                    elif self.elementXML.__contains__(self.ZONDSEND):
+                        print('ZONDSEND ' + file)
+                        self.ui.textEdit.append('ZONDSEND ' + file)
+                    elif self.elementXML.__contains__(self.ZVPSEND):
+                        print('ZVPSEND ' + file)
+                        self.ui.textEdit.append('ZVPSEND ' + file)
 
 
 if __name__ == '__main__':
