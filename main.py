4cad0b5ce3882cb371cbb57004c13411564f0029
ShilkinYuV <51208697+ShilkinYuV@users.noreply.github.com>
2022-01-19 14:57:40 +0300

    obed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt6.QtGui import QIcon
 from mainapp import Ui_MainWindow
 from xml.dom import minidom
 import os
+import shutil
 
 
 class OperoPDS(QtWidgets.QMainWindow):
@@ -16,6 +17,7 @@ class OperoPDS(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.isASFK = 'D:\\isASFK'
         self.isPUDS = 'D:\\isPUDS'
+        self.inBANK = 'D:\\OEV\\Exg\\cli'
         self.OTVSEND = 'RDI0NCB4bWxucz0idXJuOmNici1ydTplZDp2Mi4wIiBFRE5vPSI'
         self.OTZVSEND = 'RDI3NSB4bWxucz0idXJuOmNici1ydTplZDp2Mi4wIiBFRE5vPSI'
         self.PESSEND = 'YWNrZXRFSUQgeG1sbnM9InVybjpjYnItcnU6ZWQ6djIuMCIgRURObz0i'
@@ -23,7 +25,53 @@ class OperoPDS(QtWidgets.QMainWindow):
         self.ZINFSEND = 'RDI0MCBFREF1dGhvcj0i'
         self.ZONDSEND = 'RDk5OSBDcmVhdGlvbkRhdGVUaW1l'
         self.ZVPSEND = 'RDIxMCB4bWxucz0idXJuOmNici1ydTplZDp2Mi4wIiBFRE5vPSI'
+        self.ui.chekDocuments.clicked.connect(self.checkfiles)
+        self.ui.OTVSEND.clicked.connect(lambda: self.sending(self.OTVSEND))
+        self.ui.OTZVSEND.clicked.connect(lambda: self.sending(self.OTZVSEND))
+        self.ui.PESSEND.clicked.connect(lambda: self.sending(self.PESSEND))
+        self.ui.RNPSEND.clicked.connect(lambda: self.sending(self.RNPSEND))
+        self.ui.ZINFSEND.clicked.connect(lambda: self.sending(self.ZINFSEND))
+        self.ui.ZONDSEND.clicked.connect(lambda: self.sending(self.ZONDSEND))
+        self.ui.ZVPSEND.clicked.connect(lambda: self.sending(self.ZVPSEND))
 
+    def sending(self, type):
+        files = os.listdir(self.isASFK)
+
+        isEmpty = True
+
+        for file in files:
+            myFile = self.isASFK + '\\' + file
+            mydoc = minidom.parse(myFile)
+            items = mydoc.getElementsByTagName('sen:Object')
+
+            for elem in items:
+                self.elementXML = str(elem.firstChild.data)
+                if self.elementXML.__contains__(type):
+                    self.ui.textEdit.append('Начало копирования ' + file + ' в ' + self.inBANK)
+                    isEmpty = False
+                    try:
+                        shutil.copy2(myFile, self.inBANK + '\\' + file)
+                        self.ui.textEdit.append('Копирование ' + file + ' в ' + self.inBANK + ' успешно завершено')
+                    except Exception:
+                        self.ui.textEdit.append('Копирование ' + file + ' в ' + self.inBANK + ' не удалось')
+
+                    bankfiles = os.listdir(self.inBANK)
+                    for bankfile in bankfiles:
+                        if bankfile.__contains__(file):
+                            self.ui.textEdit.append('Файл ' + file + ' присутствует в ' + self.inBANK)
+                            try:
+                               os.remove(myFile)
+                               self.ui.textEdit.append('Файл ' + file + ' удален из ' + self.isASFK)
+                            except:
+                                self.ui.textEdit.append('Не удалось удалить ' + file + ' из ' + self.isASFK)
+
+        if isEmpty:
+            sender = self.sender()
+            self.ui.textEdit.append('Корневой каталог не содержит файлов ' + sender.text())
+
+
+
+    def checkfiles(self):
         files = os.listdir(self.isASFK)
         print(files)
 
@@ -35,19 +83,26 @@ class OperoPDS(QtWidgets.QMainWindow):
             for elem in items:
                 self.elementXML = str(elem.firstChild.data)
                 if self.elementXML.__contains__(self.OTVSEND):
-                    print('OTVSEND ' + myFile)
+                    print('OTVSEND ' + file)
+                    self.ui.textEdit.setPlainText('OTVSEND ' + file)
                 elif self.elementXML.__contains__(self.OTVSEND):
-                    print('OTZVSEND' + myFile)
+                    print('OTZVSEND ' + file)
+                    self.ui.textEdit.append('OTZVSEND ' + file)
                 elif self.elementXML.__contains__(self.PESSEND):
-                    print('PESSEND' + myFile)
+                    print('PESSEND ' + file)
+                    self.ui.textEdit.append('PESSEND ' + file)
                 elif self.elementXML.__contains__(self.RNPSEND):
-                    print('RNPSEND' + myFile)
+                    print('RNPSEND ' + file)
+                    self.ui.textEdit.append('RNPSEND ' + file)
                 elif self.elementXML.__contains__(self.ZINFSEND):
-                    print('ZINFSEND' + myFile)
+                    print('ZINFSEND ' + file)
+                    self.ui.textEdit.append('ZINFSEND ' + file)
                 elif self.elementXML.__contains__(self.ZONDSEND):
-                    print('ZONDSEND' + myFile)
+                    print('ZONDSEND ' + file)
+                    self.ui.textEdit.append('ZONDSEND ' + file)
                 elif self.elementXML.__contains__(self.ZVPSEND):
-                    print('ZVPSEND' + myFile)
+                    print('ZVPSEND ' + file)
+                    self.ui.textEdit.append('ZVPSEND ' + file)
 
 
 if __name__ == '__main__':
@@ -56,4 +111,3 @@ if __name__ == '__main__':
     application.show()
 
     sys.exit(app.exec())
-
