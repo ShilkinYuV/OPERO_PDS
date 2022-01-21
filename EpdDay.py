75e89e537ed0e98f2f7342463788ccb405afc4f5
ShilkinYuV <51208697+ShilkinYuV@users.noreply.github.com>
2022-01-21 19:18:58 +0300

    go home

--- EpdDay.py
+++ EpdDay.py
@@ -19,7 +19,22 @@ class EpdDay:
 
     def go_epd_day(self):
         print('день')
+        files = os.listdir(self.isBANK)
+        count_files_before = (len(files) - 2)
+        print(count_files_before)
+
         try:
            os.system('D:\\OEV\\Exg\\rcv\\unb64_rabis.exe *.* D:\\OEV\\Exg\\rcv >> D:\\OEV\\Exg\\rcv\\logs\\decod.log')
         except Exception:
             print('Ошибка ебучая')
+
+        files = os.listdir(self.isBANK)
+        count_files_after = (len(files) - 2)
+        print(count_files_after)
+
+        if count_files_after - count_files_before == count_files_before:
+            self.my_window.ui.textEdit.append('Расшифровка файлов успешно завершена')
+            logging.info('Расшифровка файлов успешно завершена')
+        else:
+            self.my_window.ui.textEdit.append('Не удалось расшифровать все файлы, из ' + count_files_after + " " + "Расшифровано " + (count_files_after - count_files_before))
+            logging.error('Не удалось расшифровать все файлы, из ' + count_files_after + " " + "Расшифровано " + (count_files_after - count_files_before))
